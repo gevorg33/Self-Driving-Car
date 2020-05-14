859b194cc57380db6060959c20219f2b9a050a22
gevorg33 <55300395+gevorg33@users.noreply.github.com>
2020-05-14 19:58:21 +0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -64,6 +64,7 @@ class Dqn():
         action = probs.multinomial(num_samples=1)
         return action.data[0,0]
     
+    
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
         next_outputs = self.model(batch_next_state).detach().max(1)[0]
